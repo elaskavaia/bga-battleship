ffa8ac529e9f83749e6e4bd13c846103db309317
elaskavaia <elaskavaia@Porcupine>
2017-09-12 19:08:41 -0400

    fixed API_Ext class

--- modules/APP_Extended.php
+++ modules/APP_Extended.php
@@ -1,80 +1,104 @@
 <?php
 
+/**
+ * This class contains more useful method which is missing from Table class.
+ * To use extend this instead instead of Table, i.e
+ *
+ <code>
+ require_once (APP_GAMEMODULE_PATH . 'module/table/table.game.php');
+ require_once ('modules/tokens.php');
+ require_once ('modules/APP_Extended.php');
+ 
+ class BattleShip extends APP_Extended {
+ }
+ </code>
+ *
+ */
 abstract class APP_Extended extends Table {
     
+    function __construct() {
+        parent::__construct();
+        self::initGameStateLabels(
+                array (
+                        "move_nbr" => 6,
+                ));
+        $this->gameinit = false;
+    }
+    
     // ------ ERROR HANDLING ----------
-
+    
     /**
      * This will throw an exception if condition is false.
      * The message should be translated and shown to the user.
      *
      * @param $log string
-     *            is
-     *            server side log message, no translation needed
+     *            user side log message, translation is needed, use self::_() when passing string to it
      * @throws BgaUserException
      */
     function userAssertTrue($message, $cond = false, $log = "") {
         if ($cond)
             return;
-        if ($log)
-            $this->warn($message . " " . $log);
-        throw new BgaUserException($message);
+            if ($log)
+                $this->warn($message . " " . $log);
+                throw new BgaUserException($message);
     }
-
+    
     /**
      * This will throw an exception if condition is false.
      * This only can happened if user hacks the game, client must prevent this
      *
-     * @param $log
+     * @param $log string
      *            server side log message, no translation needed
      * @throws BgaUserException
      */
     function systemAssertTrue($log, $cond = false) {
         if ($cond)
             return;
-        $move = $this->getGameStateValue('move_nbr');
-        //trigger_error("bt") ;
-        //$bt = debug_backtrace();
-        //$this->dump('bt',$bt);
-        $this->error("Internal Error during move $move: $log|");
-        //throw new feException($log);
-        throw new BgaUserException(self::_("Internal Error. That should not have happened. Please raise a bug. ") . $log); // TODO remove
+            $move = $this->getGameStateValue('move_nbr');
+            //trigger_error("bt") ;
+            //$bt = debug_backtrace();
+            //$this->dump('bt',$bt);
+            $this->error("Internal Error during move $move: $log|");
+            //throw new feException($log);
+            throw new BgaUserException(self::_("Internal Error. That should not have happened. Please raise a bug. "));
     }
     
     // ------ NOTIFICATIONS ----------
-
+    
     function notifyWithName($type, $message = '', $args = null, $player_id = -1) {
-        if ($this->gameinit)
-            return;
         if ($args == null)
             $args = array ();
-        $this->systemAssertTrue("Invalid notification signature", is_array($args));
-        if ($player_id == - 1)
-            $player_id = $this->getActivePlayerId();
-        $args ['player_id'] = $player_id;
-        if ($message) {
-            $player_name = $this->getPlayerName($player_id);
-            $args ['player_name'] = $player_name;
-        }
-        if (isset($args ['_private'])) {
-            unset($args ['_private']);
-            $this->notifyPlayer($player_id, $type, $message, $args);
-        } else {
-            $this->notifyAllPlayers($type, $message, $args);
-        }
+            $this->systemAssertTrue("Invalid notification signature", is_array($args));
+            if ($player_id == - 1)
+                $player_id = $this->getActivePlayerId();
+                $args ['player_id'] = $player_id;
+                if ($message) {
+                    $player_name = $this->getPlayerName($player_id);
+                    $args ['player_name'] = $player_name;
+                }
+                if (isset($args ['_private'])) {
+                    unset($args ['_private']);
+                    $this->notifyPlayer($player_id, $type, $message, $args);
+                } else {
+                    $this->notifyAllPlayers($type, $message, $args);
+                }
     }
     
     // ------ PLAYERS ----------
     
     /**
      *
-     * @return first player in natural player order
+     * @return integer first player in natural player order
      */
     function getFirstPlayer(){
         $table = $this->getNextPlayerTable();
         return $table[0];
     }
     
+    /**
+     *
+     * @return string hex color as in players table for the player with $player_id
+     */
     function getPlayerColor($player_id) {
         if (! isset($this->players_basic)) {
             $this->players_basic = $this->loadPlayersBasicInfos();
@@ -84,7 +108,10 @@ abstract class APP_Extended extends Table {
         }
         return $this->players_basic [$player_id] ['player_color'];
     }
-    
+    /**
+     *
+     * @return string player name based on $player_id
+     */
     function getPlayerName($player_id) {
         if (! isset($this->players_basic)) {
             $this->players_basic = $this->loadPlayersBasicInfos();
@@ -95,6 +122,10 @@ abstract class APP_Extended extends Table {
         return $this->players_basic [$player_id] ['player_name'];
     }
     
+    /**
+     *
+     * @return integer player id based on hex $color
+     */
     function getPlayerIdByColor($color) {
         if (! isset($this->players_basic)) {
             $this->players_basic = $this->loadPlayersBasicInfos();
@@ -111,6 +142,10 @@ abstract class APP_Extended extends Table {
         return $this->player_colors [$color];
     }
     
+    /**
+     *
+     * @return integer player position (as player_no) from database
+     */
     function getPlayerPosition($player_id) {
         if (! isset($this->players_basic)) {
             $this->players_basic = $this->loadPlayersBasicInfos();
@@ -121,6 +156,10 @@ abstract class APP_Extended extends Table {
         return $this->players_basic [$player_id] ['player_no'];
     }
     
+    /**
+     *
+     * @return integer number of players
+     */
     public function getNumPlayers() {
         if (! isset($this->players_basic)) {
             $this->players_basic = $this->loadPlayersBasicInfos();
@@ -128,6 +167,10 @@ abstract class APP_Extended extends Table {
         return count($this->players_basic);
     }
     
+    /**
+     *
+     * Change activate player, also increasing turns_number stats and giving extra time
+     */
     function setNextActivePlayerCustom($next_player_id) {
         $this->giveExtraTime($next_player_id);
         $this->incStat(1, 'turns_number', $next_player_id);
